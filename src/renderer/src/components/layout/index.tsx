30183d1657bd9b3f7c052d081c7e283bbf3a6653
Ali Asad <aliasad.3128@gmail.com>
2024-12-10 11:26:18 +0330

    feat: implement application main layout

---
+++ src/renderer/src/components/layout/index.tsx
@@ -0,0 +1,27 @@
+import { Outlet } from "react-router-dom";
+import Header from "./header";
+import Sidebar from "./sidebar";
+import DeviceStatus from "@renderer/components/widget/device-status";
+import Frequency from "../widget/frequency";
+import SelectMode from "../widget/select-mode";
+import AnalyticsReport from "../widget/analytics-report";
+
+const Layout = (): JSX.Element => {
+  return (
+    <div className="flex">
+      <Header />
+      <Sidebar />
+      <div className="h-screen max-h-screen w-full space-y-5 overflow-y-auto p-5">
+        <h1 className="text-3xl font-bold">Config Setup</h1>
+        <div className="grid grid-cols-1 gap-10 xl:grid-cols-2">
+          <Frequency />
+          <SelectMode />
+        </div>
+        <Outlet />
+        <AnalyticsReport />
+      </div>
+      <DeviceStatus />
+    </div>
+  );
+};
+export default Layout;
---
+++ src/renderer/src/components/layout/sidebar.tsx
@@ -0,0 +1,58 @@
+import { cn } from "@renderer/lib/utils";
+import { useMemo } from "react";
+import { NavLink } from "react-router-dom";
+
+const Sidebar = (): JSX.Element => {
+  const navbarItems = useMemo(
+    () => [
+      {
+        label: "Single Tone",
+        href: "/single-tone",
+      },
+      {
+        label: "Multi Tone",
+        href: "/multi-tone",
+      },
+      {
+        label: "Sweep",
+        href: "/sweep",
+      },
+      {
+        label: "Barrage",
+        href: "/barrage",
+      },
+      {
+        label: "Filtered Noise",
+        href: "/filtered-noise",
+      },
+      {
+        label: "Delay/Doppler",
+        href: "/delay-doppler",
+      },
+    ],
+    [],
+  );
+
+  return (
+    <div className="h-screen w-64 shrink-0 overflow-y-auto bg-gradient-to-t from-accent-foreground from-10% to-primary px-3 py-5 ring">
+      <h2 className="text-2xl font-bold text-accent">USB Agent</h2>
+      <div className="mt-20 space-y-5">
+        {navbarItems.map((item, index) => (
+          <NavLink
+            key={index}
+            to={item.href}
+            className={({ isActive }) =>
+              cn(
+                "block rounded-md px-3 py-2 text-lg text-accent transition-colors hover:bg-primary-foreground/20",
+                isActive && "pointer-events-none bg-primary-foreground/30",
+              )
+            }
+          >
+            {item.label}
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Sidebar;
