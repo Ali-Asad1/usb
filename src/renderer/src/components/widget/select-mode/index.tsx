8f3fbce912fdb605c1fdb1240cda3937c4b4f087
Ali Asad <aliasad.3128@gmail.com>
2024-12-13 00:44:52 +0330

    feat: add on and off mode for pulse mode

--- src/renderer/src/components/widget/select-mode/index.tsx
+++ src/renderer/src/components/widget/select-mode/index.tsx
@@ -1,9 +1,13 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@renderer/components/ui/card";
+import { Popover, PopoverContent, PopoverTrigger } from "@renderer/components/ui/popover";
+import { Switch } from "@renderer/components/ui/swtich";
 import { ToggleGroup, ToggleGroupItem } from "@renderer/components/ui/toggle-group";
+import { useDeviceMode } from "@renderer/hooks/state/use-device-mode";
 import { useDeviceSettings } from "@renderer/hooks/state/use-device-settings";
 
 const SelectMode = (): JSX.Element => {
   const { data, onChange } = useDeviceSettings();
+  const { data: mode, onChange: onModeChange } = useDeviceMode();
 
   return (
     <Card>
@@ -17,7 +21,10 @@ const SelectMode = (): JSX.Element => {
           type="single"
           value={data.NOISES.PSGMODE}
           onValueChange={(value) => {
-            onChange({ ...data, NOISES: { ...data.NOISES, PSGMODE: value } });
+            if (value) {
+              onChange({ ...data, NOISES: { ...data.NOISES, PSGMODE: value ? value : data.NOISES.PSGMODE } });
+              onModeChange({ ...mode, mode: value });
+            }
           }}
         >
           <ToggleGroupItem value="0" className="data-[state=on]:bg-primary data-[state=on]:text-accent">
@@ -26,11 +33,33 @@ const SelectMode = (): JSX.Element => {
           <ToggleGroupItem value="1" className="data-[state=on]:bg-primary data-[state=on]:text-accent">
             CT
           </ToggleGroupItem>
-          <ToggleGroupItem value="2" className="data-[state=on]:bg-primary data-[state=on]:text-accent">
-            Pulses
+          <ToggleGroupItem value="2" className="px-0 data-[state=on]:bg-primary data-[state=on]:text-accent">
+            <Popover>
+              <PopoverTrigger className="size-full px-3">Pulses</PopoverTrigger>
+              <PopoverContent className="flex w-fit items-center gap-x-5">
+                Deterministic
+                <Switch
+                  checked={mode.deterministic}
+                  onCheckedChange={(checked) => {
+                    onModeChange({ ...mode, deterministic: checked });
+                  }}
+                />
+              </PopoverContent>
+            </Popover>
           </ToggleGroupItem>
-          <ToggleGroupItem value="3" className="data-[state=on]:bg-primary data-[state=on]:text-accent">
-            R.Pulse
+          <ToggleGroupItem value="3" className="px-0 data-[state=on]:bg-primary data-[state=on]:text-accent">
+            <Popover>
+              <PopoverTrigger className="size-full px-3">R.Pulses</PopoverTrigger>
+              <PopoverContent className="flex w-fit items-center gap-x-5">
+                Stochastic
+                <Switch
+                  checked={mode.stochastic}
+                  onCheckedChange={(checked) => {
+                    onModeChange({ ...mode, stochastic: checked });
+                  }}
+                />
+              </PopoverContent>
+            </Popover>
           </ToggleGroupItem>
         </ToggleGroup>
       </CardContent>
