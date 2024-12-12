2a753a095033b65fc3e292eb9bb835643100a55a
Ali Asad <aliasad.3128@gmail.com>
2024-12-13 01:00:19 +0330

    feat: add `LOFRQCY` input

--- src/renderer/src/components/widget/frequency/index.tsx
+++ src/renderer/src/components/widget/frequency/index.tsx
@@ -1,4 +1,5 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@renderer/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@renderer/components/ui/card";
+import { Input } from "@renderer/components/ui/input";
 import { Slider } from "@renderer/components/ui/slider";
 import { useDeviceSettings } from "@renderer/hooks/state/use-device-settings";
 import { cn } from "@renderer/lib/utils";
@@ -21,11 +22,9 @@ const Frequency = (): JSX.Element => {
           max={70}
           step={1}
         />
-      </CardContent>
-      <CardFooter>
         <div
           className={cn(
-            "w-full rounded-md bg-primary px-5 py-1 font-semibold text-accent transition-colors",
+            "mt-5 w-full rounded-md bg-primary px-5 py-1 font-semibold text-accent transition-colors",
             data.LOFATT.TXATTEN >= 50
               ? "bg-red-500"
               : data.LOFATT.TXATTEN >= 30
@@ -35,7 +34,22 @@ const Frequency = (): JSX.Element => {
         >
           {data.LOFATT.TXATTEN} dBm
         </div>
-      </CardFooter>
+        <div className="mt-5">
+          <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+            Local Oscillator frequency (Hz)
+          </label>
+          <Input
+            type="number"
+            placeholder="Local Oscillator (LO) frequency (Hz)"
+            min={70}
+            max={6000000000}
+            value={data.LOFATT.LOFRQCY}
+            onChange={(e) => {
+              onChange({ ...data, LOFATT: { ...data.LOFATT, LOFRQCY: e.target.value } });
+            }}
+          />
+        </div>
+      </CardContent>
     </Card>
   );
 };
