a6fe4be78d839e374808d384a945c7482f8a774a
maleki <mhmm69923@gmail.com>
2024-12-23 21:44:33 +0330

    add spase in packet class

--- src/renderer/src/helper/packet.ts
+++ src/renderer/src/helper/packet.ts
@@ -1,3 +1,3 @@
 export const createPacket = (method: "SET" | "GET", attribute: string, type: string, data: string | number) => {
-  return `START!*${method}!!!$$${type}*${attribute}$$${data.toString().padEnd(10, "!")}$$RESERVED$$ENDOFPKT\n`;
+  return `START! *${method}!!!$$${type} *${attribute}$$${data.toString().padEnd(10, "!")}$$RESERVED$$ENDOFPKT\n`;
 };
