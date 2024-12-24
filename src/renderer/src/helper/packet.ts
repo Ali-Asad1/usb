bbe0dcc4a88ef5894496302124b966d0bbf5b8ba
Ali Asad <aliasad.3128@gmail.com>
2024-12-24 17:14:56 +0330

    feat: implement function for parse packet data

--- src/renderer/src/helper/packet.ts
+++ src/renderer/src/helper/packet.ts
@@ -1,3 +1,15 @@
 export const createPacket = (method: "SET" | "GET", attribute: string, type: string, data: string | number) => {
   return `START!*${method}!!!$$${type}*${attribute}$$${data.toString().padEnd(10, "!")}$$RESERVED$$ENDOFPKT\n`;
 };
+
+export const parsePacket = (packet: string) => {
+  const match = packet.match(/\*SET!!!\$\$(.*?)\s\*(.*?)\$\$(.*?)\$\$/);
+  if (match) {
+    return {
+      type: match[1],
+      attribute: match[2],
+      data: match[3].replace(/!+/g, ""),
+    };
+  }
+  return null;
+};
