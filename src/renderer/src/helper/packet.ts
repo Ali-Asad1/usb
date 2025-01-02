c5b51a31c55b91f3621af566b6a0464ae4dcc949
Ali Asad <aliasad.3128@gmail.com>
2025-01-03 01:39:26 +0330

    refactor: adjust `createPacket` parameter order for improved clarity

--- src/renderer/src/helper/packet.ts
+++ src/renderer/src/helper/packet.ts
@@ -1,4 +1,4 @@
-export const createPacket = (method: "SET" | "GET", attribute: string, type: string, data: string | number) => {
+export const createPacket = (method: "SET" | "GET", type: string, attribute: string, data: string | number) => {
   return `*START! *${method}!!!$$${type} *${attribute}$$${data.toString().padEnd(10, "!")}$$RESERVED$$ENDOFPKT\n`;
 };
 
